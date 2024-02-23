98b4b89af4523f652705cd027b5c3ef58c362dae
kaisky89 <kaisky89@gmail.com>
2024-02-23 19:35:39 +0000

    Deploying to gh-pages from @ k5-leitertraining/k5-ghl-utils@4c7870797d61203f2c5b77e600dd5b44038d57f4 🚀

--- k5-ghl-utils.js
+++ k5-ghl-utils.js
@@ -1,86 +1,89 @@
-const y = (r, e) => {
-  let t;
-  return (...n) => {
-    clearTimeout(t), t = window.setTimeout(() => {
-      r(...n);
+const h = (r, e) => {
+  let n;
+  return (...t) => {
+    clearTimeout(n), n = window.setTimeout(() => {
+      r(...t);
     }, e);
   };
-}, d = ({
+}, y = ({
   shouldDisplayError: r,
   element: e,
-  errorMessage: t
+  errorMessage: n
 }) => {
-  var o, s;
-  (o = e.parentElement) != null && o.querySelector(".course-already-bought-error") || e.insertAdjacentHTML(
+  var s, o;
+  (s = e.parentElement) != null && s.querySelector(".course-already-bought-error") || e.insertAdjacentHTML(
     "afterend",
-    `<div class="error course-already-bought-error">${t}</div>`
+    `<div class="error course-already-bought-error">${n}</div>`
   );
-  const n = (s = e.parentElement) == null ? void 0 : s.querySelector(
+  const t = (o = e.parentElement) == null ? void 0 : o.querySelector(
     ".course-already-bought-error"
   );
-  n && (n.style.cssText = r ? "" : "display: none;");
-}, b = "opacity: 0.5; cursor: not-allowed;";
-let u;
-const m = (r) => {
-  var t, n;
-  u || (u = ((n = (t = document.querySelector("[type=submit]")) == null ? void 0 : t.style) == null ? void 0 : n.cssText) ?? "");
+  t && (t.style.cssText = r ? "" : "display: none;");
+}, p = "opacity: 0.5; cursor: not-allowed;";
+let c;
+const b = (r) => {
+  var n, t;
+  c || (c = ((t = (n = document.querySelector("[type=submit]")) == null ? void 0 : n.style) == null ? void 0 : t.cssText) ?? "");
   const e = document.querySelector("[type=submit]");
-  return e ? (e.disabled = r, e.style.cssText = r ? u + b : u, e) : null;
+  return e ? (e.disabled = r, e.style.cssText = r ? c + p : c, e) : null;
 };
-let i;
-const p = async ({
+let l;
+const E = async ({
   target: r,
   errorMessage: e,
-  courseId: t
+  courseId: n,
+  localDev: t = !1
 }) => {
   if (!(r instanceof HTMLInputElement))
     return;
-  const n = r.value;
-  if (i == null || i.abort(), !n)
+  const s = r.value;
+  if (l == null || l.abort(), !s)
     return;
-  i = new AbortController();
-  const o = i.signal, s = await fetch(
-    `https://k5-leitertraining.de/.netlify/functions/is-registered?email=${n}&course=${t}`,
+  l = new AbortController();
+  const o = l.signal, u = await fetch(
+    `${t ? "http://localhost:9999" : "https://k5-leitertraining.de"}/.netlify/functions/is-registered?email=${s}&course=${n}`,
     { signal: o }
   ).then(
-    (c) => c.json()
-  ).catch((c) => {
-    console.log(c);
-  }), l = !!(s != null && s.isAlreadyRegistered);
-  r.setCustomValidity(l ? e : "");
-  const a = m(l);
-  a && d({
-    shouldDisplayError: l,
-    element: a,
+    (d) => d.json()
+  ).catch((d) => {
+    console.log(d);
+  }), i = !!(u != null && u.isAlreadyRegistered);
+  r.setCustomValidity(i ? e : "");
+  const m = b(i);
+  m && y({
+    shouldDisplayError: i,
+    element: m,
     errorMessage: e
-  }), d({
-    shouldDisplayError: l,
+  }), y({
+    shouldDisplayError: i,
     element: r,
     errorMessage: e
   });
-}, E = y(p, 500), h = ({
+}, f = h(E, 500), g = ({
   errorMessage: r = "Mit dieser E-Mail Adresse wurde dieser Kurs bereits gebucht oder eine Ortsgruppe erstellt. Bitte verwende eine andere E-Mail Adresse.",
-  courseId: e = "generalCourseParticipantOrCommunityMember"
+  courseId: e = "generalCourseParticipantOrCommunityMember",
+  localDev: n = !1
 } = {}) => {
   var o;
-  (o = document.querySelector('[name="email"]')) == null || o.addEventListener("input", (s) => {
-    s.target && (m(!0), E({
-      target: s.target,
+  (o = document.querySelector('[name="email"]')) == null || o.addEventListener("input", (a) => {
+    a.target && (b(!0), f({
+      target: a.target,
       errorMessage: r,
-      courseId: e
+      courseId: e,
+      localDev: n
     }));
   });
-  const t = document.querySelector('[name="email"]'), n = t == null ? void 0 : t.value;
+  const t = document.querySelector('[name="email"]'), s = t == null ? void 0 : t.value;
   t == null || t.dispatchEvent(
     new Event("input", {
       bubbles: !0,
       cancelable: !0,
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      value: n
+      value: s
     })
   );
 };
 export {
-  h as validateEmail
+  g as validateEmail
 };
