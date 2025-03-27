ae73ec39b7bd7bf0d6d95e4612ef6061ce243c44
Kai Wittmann <kai@heart-co.de>
2025-03-27 10:37:20 +0100

    disable navigation sync for now

--- src/utils/navbarControl/index.ts
+++ src/utils/navbarControl/index.ts
@@ -2,10 +2,13 @@ import { getCustomMenuItems } from './data/getCustomMenuItems'
 import { loadNavbarStyle } from './style/loadNavbarStyle'
 import { constantlyApplyNavigation } from './tools/applyNavigation'
 
+const DISABLE_SYNC = true
+
 export const navbarControl = async ({ localDev = false } = {}) => {
   await loadNavbarStyle()
 
-  const menuItems = await getCustomMenuItems({ localDev })
+  if (DISABLE_SYNC) return
 
+  const menuItems = await getCustomMenuItems({ localDev })
   constantlyApplyNavigation(menuItems)
 }
